42d6cd2ce63a9730aaafe31e1dc66dea429e5989
Avislux <gameguy94@hotmail.com>
2016-07-27 23:00:12 -0700

    gitignore excludes standard assets. Made basic UI panel with closing functionality

---
+++ GamePitch2016/Assets/Scripts/Inventory/CloseInventory.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloseInventory : MonoBehaviour {
+    
+    public GameObject parentObject;
+    public void close()
+    {
+        parentObject = GameObject.Find("Panel");
+        Debug.Log("Close button clicked");
+    
+        parentObject.SetActive(false);
+        //InventoryUi.Panel.hide();
+        // GUI.Window();
+    }
+}
