270bcc2280bafd5570b3dd94ff7a6e2dd958594b
klturchik <kturchik@sbcglobal.net>
2016-08-20 22:44:20 -0700

    First Animations and AI for Enemies
    Added Sprites, Animators, and basic AI for Locker Monster and Football
    Monster
    
    Fixed a bug with Physics 2D settings where trigger colliders were being
    treated as normal colliders
    Fixed a bug where arrow key input was inverted

---
+++ GamePitch2016/Assets/Scripts/Enemy/lockerAttack.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lockerAttack : MonoBehaviour {
+
+	Animator enemyAnimator;
+
+	//attacking
+	void Start()
+	{
+		enemyAnimator = GetComponentInChildren<Animator>();
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "Player"){
+			enemyAnimator.SetBool("isAttacking", true);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if(other.tag == "Player"){
+			enemyAnimator.SetBool("isAttacking", false);
+		}
+	}
+}
