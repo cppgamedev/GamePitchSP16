b9f33d3521383af520a1d498c35829dd7970533a
Avislux <gameguy94@hotmail.com>
2016-08-27 00:21:04 -0700

    Made a healthbar. added script to player character.

--- GamePitch2016/Assets/Scripts/Player/PlayerStats.cs
+++ GamePitch2016/Assets/Scripts/Player/PlayerStats.cs
@@ -3,16 +3,18 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class PlayerStats : MonoBehaviour {
-
+    public static PlayerStats Instance;
     public float health;
     public const float maxHealth = 3;
 
 	void Awake ()
     {
+        Instance = this;
         health = maxHealth;
+        
 	}
 	
-    public float getHelath()
+    public float getHealth()
     {
         return health;
     }
@@ -20,13 +22,15 @@ public class PlayerStats : MonoBehaviour {
 	public void removeHealth (float amount)
     {
         health -= amount;
-        if(health <= 0)
+        Debug.Log("Damaged. Health is now: " + health);
+        if (health <= 0)
         {
 			die();
         }
 	}
 	
 	public void die() {
+        Debug.Log("Died, restarting");
 		// add timer so level doesn't reset right away?
 		 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 	}
